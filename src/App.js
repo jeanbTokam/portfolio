7c0c7ef2f9269f86ed16ba83b965f66cde1ebc1d
jbtokam <jeanbTokam@github.com>
2024-04-03 13:25:13 -0400

    Added new routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Courses from './pages/Courses';
+import Contact from './pages/Contact';
 
 function App() {
-
   return (
-    <div className="App">
-         
-         <h1>PORTFOLIO</h1>
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
